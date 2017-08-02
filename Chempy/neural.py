de89de6fa42a00c3727f4c0b37b24cdf7124e0b0
Oliver Philcox <ohep2@cam.ac.uk>
2017-08-02 17:37:24 +0200

    neural plots
    Former-commit-id: 5c67292e6db37c0e8f77370dd23d6623c185f624

--- Chempy/neural.py
+++ Chempy/neural.py
@@ -2,6 +2,7 @@ from Chempy.parameter import ModelParameters
 from Chempy.cem_function import posterior_function_returning_predictions
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 
 def training_data():
 	""" Function to create neural network training dataset for Chempy data. 
@@ -56,7 +57,7 @@ def training_data():
 	# Save abundance table
 	np.save(directory+'training_abundances.npy', training_abundances)
 
-	return 0
+	return None
 	
 def verification_and_testing():
 	""" This will create the verification and testing data-sets for use with the neural network.
@@ -97,14 +98,15 @@ def verification_and_testing():
  		# Save abundance table
 		np.save("Neural/"+name+"_abundances.npy",model_abundances)
 		
-	return 0
+	return None
 
-def create_network():
+def create_network(Plot=True):
 	""" Function to create and train the neural network - this overwrites any previous network.
 	
 	Outputs:
 		epochs - Training epoch number (outputted each 10 epochs)
 		losslog - loss value for each 10 epochs
+		Plot of loss against epoch (if Plot=True)
 		
 		Neural/neural_model.npz - Saved .npz file with model weights
 	"""
@@ -170,7 +172,14 @@ def create_network():
 				b_array_0=model_numpy[1],
 				w_array_1=model_numpy[2],
 				b_array_1=model_numpy[3])
-				
+	
+	if Plot==True:
+		plt.plot(epoch,losslog)
+		plt.ylabel("L1 Loss value")
+		plt.xlabel("Epoch")
+		plt.title("Loss plot for learning rate = %s" %(a.learning_rate))	
+		plt.savefig('Neural/lossplot.png')
+			
 	return epoch, losslog
 
 
@@ -202,7 +211,135 @@ def neural_output(test_input):
 	output = np.dot(w_array_1, hidden)+b_array_1
 	
 	return output
+
+def neural_errors(dataset):
+	""" Calculate median absolute error between Chempy and neural network for each set of parameters.
+	Input is the name of the dataset: 'verif' or 'test'
+	"""
 	
+	# Load abundances
+	model_abundances = np.load('Neural/'+dataset+'_abundances.npy')
+	params = np.load('Neural/'+dataset+'_param_grid.npy')
+	
+	# Calculate absolute model error
+	error=[]
+	for i in range(len(params)):
+		predicted_abundances = neural_output(params[i])
+		error.append(np.absolute(predicted_abundances-model_abundances[i]))
+
+	return np.median(error,axis=1)
+	
+def neural_corner_plot():
+	""" This function plots a corner plot of the model parameters, with colors showing the median neural network error.
+	The error corresponding to max color can be changed by the color_max parameter.
+	
+	Output:
+		corner_parameter_plot.png saved in Neural/ directory
+	"""
+	
+	from matplotlib import cm
+
+	a=ModelParameters()	
+
+	# Load datasets
+	data_tr = np.load('Neural/training_param_grid.npy')
+	data_v = np.load('Neural/verif_param_grid.npy')
+
+	param_error = neural_errors('verif')
+	
+	# Initialize plot
+	plt.clf()
+	text_size = 12
+	plt.rc('font', family='serif',size = text_size)
+	plt.rc('xtick', labelsize=text_size)
+	plt.rc('ytick', labelsize=text_size)
+	plt.rc('axes', labelsize=text_size, lw=1.0)
+	plt.rc('lines', linewidth = 1)
+	plt.rcParams['ytick.major.pad']='8'
+	plt.rcParams['text.latex.preamble']=[r"\usepackage{libertine}"]
+	params = {'text.usetex' : True,
+          'font.family' : 'libertine',
+          'text.latex.unicode': True,
+          }
+	plt.rcParams.update(params)
+	parameter_names = [r'$\alpha_\mathrm{IMF}$',r'$\log_{10}(\mathrm{N_{Ia}})$',
+                   r'$\log_{10}(\tau_\mathrm{Ia})$',r'$\log_{10}(\mathrm{SFE})$',
+                   r'$\log_{10}(\mathrm{SFR_{peak}})$',r'x_{out}']
 
 	
+	# Plot settings
+	fig,axes = plt.subplots(nrows = len(a.p0), ncols = len(a.p0),figsize=(14.69,8.0),dpi=300)
+	alpha = 0.5
+	lw=2 # Linewidth
+	left = 0.1 # Left side of subplots
+	right = 0.8 # Right side
+	bottom = 0.075
+	top = 0.97
+	wspace = 0.0 # blankspace width between subplots
+	hspace = 0.0 # blankspace height between subplots
+	color_max = 0.05
+	plt.subplots_adjust(left=left,bottom=bottom,right=right,top=top,wspace=wspace,hspace=hspace)
+
+	# Create plot
+	for i in range(len(a.p0)):
+		for j in range(len(a.p0)):
+			axes[i,j].locator_params(nbins=4)
+			if j==1:
+				axes[i,j].locator_params(nbins=4)
+			if i==j:
+				counts,edges = np.histogram(np.asarray(data_v[:,j]),bins=10)
+				max_count = float(np.max(counts))
+				counts = np.divide(counts,max_count)
+				median = np.zeros(len(edges)-1)
+				for k in range(len(edges)-1):
+					choice = np.logical_and(np.greater(data_v[:,j],edges[k]),np.less(data_v[:,j],edges[k+1]))
+					error=np.extract(choice,param_error)
+					if len(error) != 0:
+						median[k] = np.median(error)
+				colors = cm.plasma(median/color_max)
+				axes[i,j].bar(left = edges[:-1], height=counts, width = edges[1]-edges[0],
+									color=colors,alpha=alpha, linewidth=0)
+				axes[i,j].set_xlim(min(data_v[:,j]),max(data_v[:,j]))
+				axes[i,j].set_ylim(0,1.05)
+				if j !=0:
+					plt.setp(axes[i,j].get_yticklabels(), visible=False)
+				axes[i,j].vlines(np.percentile(data_v[:,j],15.865),axes[i,j].get_ylim()[0],axes[i,j].get_ylim()[1], color = 'k',alpha=alpha,linewidth = lw,linestyle = 'dashed')    
+				axes[i,j].vlines(np.percentile(data_v[:,j],100-15.865),axes[i,j].get_ylim()[0],axes[i,j].get_ylim()[1], color = 'k',alpha=alpha,linewidth = lw,linestyle = 'dashed')    
+				axes[i,j].vlines(np.percentile(data_v[:,j],50),axes[i,j].get_ylim()[0],axes[i,j].get_ylim()[1], color = 'k',alpha=alpha,linewidth = lw)
+			if i>j:
+				if j !=0:
+					plt.setp(axes[i,j].get_yticklabels(), visible=False)
+				P1 = axes[i,j].scatter(data_v[:,i],data_v[:,j],marker='.',alpha=0.3,
+												c=param_error,vmin=0,vmax=color_max,cmap='plasma',s=1)
+				P2 = axes[i,j].scatter(data_tr[:,i],data_tr[:,j],c='k',marker='+',s=80)
+				axes[i,j].set_xlim(min(data_tr[:,i])-0.1,max(data_tr[:,i])+0.1)
+				axes[i,j].set_ylim(min(data_tr[:,j])-0.1,max(data_tr[:,j])+0.1)
+			if j>i:
+				axes[i,j].axis('off')
+			if i == len(a.p0)-1:
+				axes[i,j].set_xlabel(parameter_names[j])
+			if j ==0:
+				axes[i,j].set_ylabel(parameter_names[i])
+			if i==2 and j == 1:
+				cplot = axes[i,j].scatter(data_v[:,i],data_v[:,j],marker='.',alpha=0.3,
+													c=param_error,vmin=0,vmax=color_max,cmap='plasma',s=1)
+	cax=fig.add_axes([0.82,0.06,0.02,0.9])
+	plt.colorbar(cplot,cax=cax)
+	plt.show()
+	fig.savefig('Neural/corner_parameter_plot.png',dpi=300,bbox_inches='tight')
+
+	return None
+
+
+
+
+
+
+
+
+
+
+
+
+
 	
