be3af4c8e6836d3db5c9ddbe4a783df9d8a482f0
Oliver Philcox <ohep2@cam.ac.uk>
2017-08-22 12:16:15 +0200

    hogg updatae
    Former-commit-id: e1adb1e4dbb08920275fa6bf16304c32138fb5a4

--- Chempy/Hogg_trial.py
+++ Chempy/Hogg_trial.py
@@ -2,7 +2,7 @@
 from Chempy.score_function import Hogg_bash
 import sys
 index = int(sys.argv[1]) # Beta index
-print('Starting process %d' %(index))
+print('Starting process %d' %(index+1))
 Hogg_bash(index)
 
 if index == 19:
