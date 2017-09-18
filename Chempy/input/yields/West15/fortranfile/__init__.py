72eac1eb43473997bcb2da764bfd919206fada2c
Oliver Philcox <ohep2@cam.ac.uk>
2017-09-18 18:02:31 +0200

    TNG
    Former-commit-id: 11a9514d640c9cbb662098d968116c9bab6496e3

--- Chempy/input/yields/West15/fortranfile/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Classes for reading and writing UNIX unformatted FORTRAN files.
-"""
-
-# TODO
-# * for general reading, load needs to be able to specify record size?
-
-from .reader import FortranReader
-from .writer import FortranWriter
